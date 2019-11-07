02b8e8362d8fe252dcf6ceba0a9a5b9f82203835
devin <Devin_feng@patozon.net>
2019-11-07 16:00:30 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -23,6 +23,11 @@ module.exports = {
   // publicPath: '/',
   // outputDir: 'dist',
 
+
+
+
+
+  
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
